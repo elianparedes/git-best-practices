3085033fd1fea5c1d30491c49ea8216850f6ef8b
Elian Paredes <elianparedes.dev@gmail.com>
2023-08-08 00:14:51 -0300

    docs: readme

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
 
-describe('Home', () => {
-  it('This header must have no bugs!', () => {
-    render(<Home />)
+describe("Home", () => {
+    it("This header must have no bugs!", () => {
+        render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /🐧🐙🐐🐸🍟😃👽🍄🍎🐛🔒🍕🌵/i,
-    })
+        const headings = screen.getAllByRole("heading");
+        const emojiHeading = headings[1]; // Get the second heading which contains the emojis
+        const headingText = emojiHeading.textContent || "";
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+        expect(headingText).not.toContain("🐛");
+    });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,51 @@
 import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
 
 import styles from "@/pages/index.module.css";
 
 export default function Home() {
+    const [isBlurred, setIsBlurred] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleBlur = () => {
+        setIsBlurred(!isBlurred);
+        setIsVisible(!isVisible);
+    };
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={toggleBlur}>
             <Head>
                 <title>No Bugs please</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
-                <h1>No bugs here!</h1>
-                <h1 className={styles.title}>🐧🐙🐐🐸🍟😃👽🍄🍎🐛🔒🍕🌵</h1>
+                <h2>{isVisible ? "No bugs please!" : "No bugs here!"}</h2>
+                <h1 className={styles.title}>
+                    <div className={styles.emojiContainer}>
+                        <span
+                            className={
+                                isBlurred ? styles.blurred : styles.unblurred
+                            }
+                        >
+                            🐧🐙🐐🐸🍟😃👽🍄🍎
+                        </span>
+                        {/* <span
+                            className={
+                                isVisible ? styles.visible : styles.invisible
+                            }
+                        >
+                            🐛
+                        </span> */}
+                        <span
+                            className={
+                                isBlurred ? styles.blurred : styles.unblurred
+                            }
+                        >
+                            🔒🍕🌵
+                        </span>
+                    </div>
+                </h1>
             </main>
         </div>
     );
